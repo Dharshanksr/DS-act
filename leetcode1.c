2b3d75d8df316c836e01dcd0034e90618c647b75
Dharshanksr <dharshau17@gmail.com>
2024-11-22 12:48:00 +0530

    create leetcode1.c

--- leetcode1.c
+++ leetcode1.c
@@ -1,9 +1,9 @@
 int romanToInt(char* s) {
-    int roman[256] = {0}; // Array to store values for each character
+    int roman[256] = {0}; 
     roman['I'] = 1; roman['V'] = 5; roman['X'] = 10;
     roman['L'] = 50; roman['C'] = 100; roman['D'] = 500; roman['M'] = 1000;
 
-    int ans = roman[s[strlen(s) - 1]]; // Start with the last character
+    int ans = roman[s[strlen(s) - 1]]; 
     
     for (int i = strlen(s) - 2; i >= 0; i--) {
         if (roman[s[i]] >= roman[s[i + 1]]) {
